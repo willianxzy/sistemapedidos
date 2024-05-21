4b93e290cdc8329c3b2aeea71df5f87f64bce913
willianxzy <willianlittig14@gmail.com>
2024-05-21 17:54:42 -0300

    commit inicial

---
+++ src/main/java/br/com/itilh/bdpedidos/sistemapedidos/SistemapedidosApplication.java
@@ -0,0 +1,13 @@
+package br.com.itilh.bdpedidos.sistemapedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemapedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemapedidosApplication.class, args);
+	}
+
+}
