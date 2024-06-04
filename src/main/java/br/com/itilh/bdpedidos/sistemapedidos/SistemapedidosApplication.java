3242b582016db3f66c80e4de6cdee2f7737e60c2
willianxzy <willianlittig14@gmail.com>
2024-06-03 21:23:34 -0300

    AULA 03/06 FINALIZADA

--- src/main/java/br/com/itilh/bdpedidos/sistemapedidos/SistemapedidosApplication.java
+++ src/main/java/br/com/itilh/bdpedidos/sistemapedidos/SistemapedidosApplication.java
@@ -2,6 +2,9 @@ package br.com.itilh.bdpedidos.sistemapedidos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SistemapedidosApplication {
@@ -9,5 +12,16 @@ public class SistemapedidosApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SistemapedidosApplication.class, args);
 	}
+
+	@Bean
+    public WebMvcConfigurer forwardToIndex() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addViewController( "/" )
+                        .setViewName("forward:/app/index.html");
+            }
+        };
+    }
 
 }
